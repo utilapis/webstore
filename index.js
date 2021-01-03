f6419c8c40d337a5952213c2c61d068bcd3138a3
Rodrigo Ibanez <rsibanez89@gmail.com>
2021-01-03 21:54:50 +1100

    05 - Hostea publicamente tu tienda online.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const repository = require("./repository");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
