efb0dde23c20c3c2755093c1781e6942825e5a41
Leonard87ce <91459304+Leonard87ce@users.noreply.github.com>
2021-10-28 19:20:03 -0500

    Update repository.js

--- repository.js
+++ repository.js
@@ -1,26 +1,24 @@
 const { google } = require("googleapis");
 
 const oAuth2Client = new google.auth.OAuth2(
-  "653250056256-ecdqbeehrajsppi4oagvinloqjkofbhd.apps.googleusercontent.com",
-  "IuqvFQQhCSHHyZfGoy6YTPV4",
+  "391925411084-d6m6hefvvb4t5jc3nged5i2vp2tdau9d.apps.googleusercontent.com",
+  "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",
   "urn:ietf:wg:oauth:2.0:oob"
 );
 
 oAuth2Client.setCredentials({
-  access_token:
-    "ya29.a0AfH6SMCKBUmfIJeFel-DKhgwuln0IiuGEP67k-72-3w9gh2FONsjA9FU11SZ19ya1Y3KeM_mssBVJDZrd9OLVz8mmRPzGY0GA1e9wBkUP2Bw8f-aetkIJP-sW4KzWLLW7Lxcz4dg8GODqGFmooICDBIeywxjI--bjRhTdLViTVM",
-  refresh_token:
-    "1//0go3CKc26E0KWCgYIARAAGBASNwF-L9Ir4kXUigSfieh7fOaGYM6VN3Ag7GjhxzXkDSUWkx4M-z4-mXGH8NalJWV7JqYFkjxiaKo",
-  scope: "https://www.googleapis.com/auth/spreadsheets",
-  token_type: "Bearer",
-  expiry_date: 1608879811848,
+    access_token: "ya29.a0ARrdaM9LlIJjLORxWWpAT2Q1x8Sd3v9q3z38k4F42RNLclz3_QMX2ko-EFPVxJgOEAYo6t8YzVnxfrwh12ixb0HElqrYi6vgZnS9Mv9fROrvQAqH4SsYbdOVQcFROjD1zyh8mQVES1VR3TmeZwr-q169LEaQ",
+    refresh_token: "1//05M4jzbYBMz10CgYIARAAGAUSNwF-L9IrrExo6Evy_Bbl2W5ww7rNdcTyh5rbD-SA24eflhjHFHl6hfzKJB9WUp6XdKxTtrtNKqc",
+    scope: "https://www.googleapis.com/auth/spreadsheets",
+    token_type: "Bearer",
+    expiry_date: 1634699523457,
 });
 
 const sheets = google.sheets({ version: "v4", auth: oAuth2Client });
 
 async function read() {
   const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: "1fO4WiyE2EP9AVT8qRLGbGrcdNPYxCAXiTBXwz3IafcM",
+    spreadsheetId: "15LrbSZOvvC1hTmaP46RClWJ672IeKisAE-oZhm2SA6w",
     range: "Products!A2:E",
   });
 
@@ -43,7 +41,7 @@ async function write(products) {
     values,
   };
   const result = await sheets.spreadsheets.values.update({
-    spreadsheetId: "1fO4WiyE2EP9AVT8qRLGbGrcdNPYxCAXiTBXwz3IafcM",
+    spreadsheetId: "15LrbSZOvvC1hTmaP46RClWJ672IeKisAE-oZhm2SA6w",
     range: "Products!A2:E",
     valueInputOption: "RAW",
     resource,
